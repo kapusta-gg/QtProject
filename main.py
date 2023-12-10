38114d3549f0dbc55221c692f0f7b3fb860e8a76
wanderlust <57632954+kapusta-gg@users.noreply.github.com>
2023-12-10 16:16:15 +0300

    Переделал открытие папки

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 import sys
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
